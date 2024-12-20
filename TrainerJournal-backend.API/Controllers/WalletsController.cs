da6d0041496176482d6416bd875b22cb1f272617
wefspy <126880105+wefspy@users.noreply.github.com>
2024-12-20 07:12:42 +0500

    fix: Authorize in WalletsController

--- TrainerJournal-backend.API/Controllers/WalletsController.cs
+++ TrainerJournal-backend.API/Controllers/WalletsController.cs
@@ -1,14 +1,11 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TrainerJournal_backend.Application.Services.Wallets;
 using TrainerJournal_backend.Application.Services.Wallets.DTOs;
-using TrainerJournal_backend.Domain.Constants;
 
 namespace TrainerJournal_backend.API.Controllers;
 
 [ApiController]
 [Route("api/wallets")]
-[Authorize(Roles = Roles.Trainer)]
 public class WalletsController(
     WalletsService walletsService)
 {
